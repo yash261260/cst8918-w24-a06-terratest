528a2798300ebfe9ec06ff493a584e8cf693ccf6
yash261260 <solankiyash922@gmail.com>
2024-02-16 21:38:48 -0500

    commit

--- test/azure_webserver_test.go
+++ test/azure_webserver_test.go
@@ -10,7 +10,7 @@ import (
 
 // You normally want to run this under a separate "Testing" subscription
 // For lab purposes you will use your assigned subscription under the Cloud Dev/Ops program tenant
-var subscriptionID string = "<your-azure-subscription-id"
+var subscriptionID string = "7a5d5ef8-3d16-4013-a13a-970f033f476e"
 
 func TestAzureLinuxVMCreation(t *testing.T) {
 	terraformOptions := &terraform.Options{
@@ -18,7 +18,7 @@ func TestAzureLinuxVMCreation(t *testing.T) {
 		TerraformDir: "../",
 		// Override the default terraform variables
 		Vars: map[string]interface{}{
-			"labelPrefix": "<your-college-id>",
+			"labelPrefix": "sola0050",
 		},
 	}
 
@@ -33,4 +33,10 @@ func TestAzureLinuxVMCreation(t *testing.T) {
 
 	// Confirm VM exists
 	assert.True(t, azure.VirtualMachineExists(t, vmName, resourceGroupName, subscriptionID))
+
+	// Confirm NiC exists and is connected to VM
+	assert.NotEmpty(t, azure.GetVirtualMachineNics(t, vmName, resourceGroupName, subscriptionID))
+
+	// Confirm the VM is running the correct Ubuntu version
+	assert.Equal(t, azure.GetVirtualMachineImage(t, vmName, resourceGroupName, subscriptionID).Version, "latest")
 }
